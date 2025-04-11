221fba18582475c9c002d3c63b9c1b7626c62409
santogarrido <santocildesgarridoraul@gmail.com>
2025-04-11 10:41:26 +0200

    Añadido los últimos javadoc y creación del javadoc

--- src/examen/CarrilBiciManager.java
+++ src/examen/CarrilBiciManager.java
@@ -6,14 +6,13 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 
 /**
- * 
- *
- * @author CádizTech
- * 
- * 
  * Esta clase nos permite gestionar los diferentes carriles bicis de la bahía de Cádiz.
  * Permitiendonos añadir tramos, mirar el estado del carril y actualizarlo, obtener un informe detallado, 
  * mirar la longitud que tiene cada carril y obtener tramos de los diferentes carriles que nos interesen.
+ * 
+ * @author CádizTech
+ * @version 2.4.0
+ * 
  */
 
 public class CarrilBiciManager {
@@ -23,7 +22,7 @@ public class CarrilBiciManager {
 
     /**
      * Constructor público que nos inicializa los mapas
-     * tramos y estadoTramos en HashMap<>()     
+     * tramos y estadoTramos en HashMap     
      */
     public CarrilBiciManager() {
         this.tramos = new HashMap<>();
@@ -106,10 +105,20 @@ public class CarrilBiciManager {
         return tramos.values().stream().mapToDouble(Double::doubleValue).sum();
     }
 
+    /**
+     * Este método nos devuelve todos los tramos.
+     * 
+     * @return todos los tramos
+     */
     public Map<String, Double> obtenerTramos() {
         return Collections.unmodifiableMap(tramos);
     }
 
+    /**
+     * Este método genera un informe de los carriles bicis
+     * 
+     * @return informe del estado de los carriles bicis
+     */
     public String generarInforme() {
         StringBuilder sb = new StringBuilder("INFORME DE CARRILES BICI - Bahía de Cádiz\n");
         sb.append("===========================================\n");
